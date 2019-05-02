1a812ffbec3b088ebb960e28bc7db4e6b16499bb
Sabbir Siddiqui <sabbir.siddiqui@infolytx.com>
2019-05-02 15:50:16 +0600

    increase coverage

--- src/SwaggerUtils.test.js
+++ src/SwaggerUtils.test.js
@@ -22,7 +22,8 @@ describe("SwaggerUtils", () => {
                         path: "/",
                         method: "get",
                         controller: () => { },
-                        doc: {}
+                        doc: {},
+                        tags: ["Engagement"]
                     }
                 ],
                 subroutes: [
@@ -34,7 +35,8 @@ describe("SwaggerUtils", () => {
                                     path: "/",
                                     method: "get",
                                     controller: () => { },
-                                    doc: {}
+                                    doc: {},
+                                    tags: ["Engagement"]
                                 },
                                 {
                                     path: "/",
@@ -69,7 +71,71 @@ describe("SwaggerUtils", () => {
             }
             const outputPath = path.resolve(__dirname, "output.json");
             SwaggerUtils.writeSwaggerJson(
-                mockRouter, outputPath, "/", sampleSwaggerInfo
+                mockRouter, outputPath, "/api", sampleSwaggerInfo
+            );
+
+            const file = fs.readFileSync(outputPath);
+            expect(file).toBeDefined();
+            fs.unlinkSync(outputPath);
+        });
+
+        it("should write json for swagger using defaults", () => {
+            const mockRouter = {
+                routes: [
+                    {
+                        path: "/",
+                        method: "get",
+                        controller: () => { },
+                        doc: {},
+                        tags: ["Engagement"]
+                    }
+                ],
+                subroutes: [
+                    {
+                        path: "/users",
+                        router: {
+                            routes: [
+                                {
+                                    path: "/",
+                                    method: "get",
+                                    controller: () => { },
+                                    doc: {},
+                                    tags: ["Engagement"]
+                                },
+                                {
+                                    path: "/",
+                                    method: "post",
+                                    controller: () => { },
+                                    doc: {}
+                                }
+                            ],
+                            subroutes: [
+                                {
+                                    path: "/:userId/posts",
+                                    router: {
+                                        routes: [
+                                            {
+                                                path: "/",
+                                                method: "get",
+                                                controller: () => { }
+                                            },
+                                            {
+                                                path: "/",
+                                                method: "post",
+                                                controller: () => { }
+                                            }
+                                        ]
+                                    }
+                                }
+                            ]
+                        },
+
+                    }
+                ]
+            }
+            const outputPath = path.resolve(__dirname, "output.json");
+            SwaggerUtils.writeSwaggerJson(
+                mockRouter, outputPath
             );
 
             const file = fs.readFileSync(outputPath);
