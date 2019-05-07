35ecf5c3a39b0d0d18969b4d97be600b327e0a42
Sabbir Siddiqui <sabbir.siddiqui@infolytx.com>
2019-05-07 09:23:46 +0600

    remove code duplication in models test

--- __tests__/Models.test.js
+++ __tests__/Models.test.js
@@ -1,59 +1,43 @@
 import Route from "../src/models/Route";
 
+function getRouteObj(method) {
+    return Route[method](
+        "/somepath", "someController", {
+            validator: "validatorFunction",
+            doc: "docJs"
+        }
+    );
+}
+
+function testRouteObj(routeObject) {
+    expect(routeObject.path).toEqual("/somepath");
+    expect(routeObject.controller).toEqual("someController");
+    expect(routeObject.validator).toEqual("validatorFunction");
+    expect(routeObject.doc).toEqual("docJs");
+}
+
 describe("Route model", () => {
     it("Should have GET function available", () => {
-        const routeObject = Route.get(
-            "/somepath", "someController", {
-                validator: "validatorFunction",
-                doc: "docJs"
-            }
-        );
+        const routeObject = getRouteObj("get")
         expect(routeObject.method).toEqual("get");
-        expect(routeObject.path).toEqual("/somepath");
-        expect(routeObject.controller).toEqual("someController");
-        expect(routeObject.validator).toEqual("validatorFunction");
-        expect(routeObject.doc).toEqual("docJs");
+        testRouteObj(routeObject);
     });
 
     it("Should have POST function available", () => {
-        const routeObject = Route.post(
-            "/somepath", "someController", {
-                validator: "validatorFunction",
-                doc: "docJs"
-            }
-        );
+        const routeObject = getRouteObj("post")
         expect(routeObject.method).toEqual("post");
-        expect(routeObject.path).toEqual("/somepath");
-        expect(routeObject.controller).toEqual("someController");
-        expect(routeObject.validator).toEqual("validatorFunction");
-        expect(routeObject.doc).toEqual("docJs");
+        testRouteObj(routeObject);
     });
 
     it("Should have PUT function available", () => {
-        const routeObject = Route.put(
-            "/somepath", "someController", {
-                validator: "validatorFunction",
-                doc: "docJs"
-            }
-        );
+        const routeObject = getRouteObj("put")
         expect(routeObject.method).toEqual("put");
-        expect(routeObject.path).toEqual("/somepath");
-        expect(routeObject.controller).toEqual("someController");
-        expect(routeObject.validator).toEqual("validatorFunction");
-        expect(routeObject.doc).toEqual("docJs");
+        testRouteObj(routeObject);
     });
 
     it("Should have DELETE function available", () => {
-        const routeObject = Route.delete(
-            "/somepath", "someController", {
-                validator: "validatorFunction",
-                doc: "docJs"
-            }
-        );
+        const routeObject = getRouteObj("delete")
         expect(routeObject.method).toEqual("delete");
-        expect(routeObject.path).toEqual("/somepath");
-        expect(routeObject.controller).toEqual("someController");
-        expect(routeObject.validator).toEqual("validatorFunction");
-        expect(routeObject.doc).toEqual("docJs");
+        testRouteObj(routeObject);
     });
 });
