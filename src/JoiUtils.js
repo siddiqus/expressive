975cf9e66c01f0db9f9e2411f0ab8db4aaef41aa
Sabbir Siddiqui <sabbir.siddiqui@shopf.co>
2020-04-07 22:56:31 +0600

    refactor for code review

--- src/JoiUtils.js
+++ src/JoiUtils.js
@@ -11,33 +11,26 @@ function _getTypeFromSchemaProperty(schemaProperty) {
     return "string";
 }
 
-function _getSchemaDefinitionForSwagger(schemaProperty) {
-    const defaultValue = schemaProperty._flags.default;
-
-    const type = _getTypeFromSchemaProperty(schemaProperty);
-
-    const schema = {
-        type
-    };
-
+function _getMinMaxFromSchemaDefition(schemaProperty) {
     let min = schemaProperty._rules.find((r) => r.name === "min");
     min = min ? min.args.limit : null;
 
     let max = schemaProperty._rules.find((r) => r.name === "max");
     max = max ? max.args.limit : null;
 
-    const multipleOf = schemaProperty._rules.find((r) => r.name === "multiple");
-    schema.multipleOf = multipleOf ? multipleOf.args.base : null;
+    return {
+        min,
+        max
+    };
+}
 
-    const pattern = schemaProperty._rules.find((r) => r.name === "pattern");
-    schema.pattern = pattern ? String(pattern.args.regex) : null;
+function _setMinMaxInSwaggerSchema(schemaProperty, type, schema) {
+    const { min, max } = _getMinMaxFromSchemaDefition(schemaProperty);
 
-    let minKey;
-    let maxKey;
-    if (["number", "integer"].includes(type)) {
-        minKey = "minimum";
-        maxKey = "maximum";
-    } else if (type === "string") {
+    let minKey = "minimum";
+    let maxKey = "maximum";
+
+    if (type === "string") {
         minKey = "minLength";
         maxKey = "maxLength";
     } else if (type === "array") {
@@ -47,30 +40,55 @@ function _getSchemaDefinitionForSwagger(schemaProperty) {
 
     schema[minKey] = min;
     schema[maxKey] = max;
+}
+
+function _setSwaggerPropsForObject(schemaProperty, schema) {
+    const requiredProperties = [];
+    const objectSchemaPropertyMap = {};
+
+    schemaProperty.$_terms.keys.forEach((objectSchema) => {
+        if (objectSchema.schema._flags.presence === "required") {
+            requiredProperties.push(objectSchema.key);
+        }
+        objectSchemaPropertyMap[objectSchema.key] = _getSchemaDefinitionForSwagger(
+            objectSchema.schema
+        );
+    });
+    schema.required = requiredProperties.length > 0 ? requiredProperties : null;
+    schema.properties = objectSchemaPropertyMap;
+}
+
+function _setSwaggerPropsForArray(schemaProperty, schema) {
+    const itemSchema = schemaProperty.$_terms.items[0];
+    const hasItemSchema = schemaProperty.$_terms.items.length > 0;
+    schema.items = hasItemSchema ? _getSchemaDefinitionForSwagger(itemSchema) : {};
+}
+
+function _getSchemaDefinitionForSwagger(schemaProperty) {
+    const defaultValue = schemaProperty._flags.default;
+
+    const type = _getTypeFromSchemaProperty(schemaProperty);
+
+    const schema = {
+        type
+    };
+
+    const multipleOf = schemaProperty._rules.find((r) => r.name === "multiple");
+    schema.multipleOf = multipleOf ? multipleOf.args.base : null;
+
+    const pattern = schemaProperty._rules.find((r) => r.name === "pattern");
+    schema.pattern = pattern ? String(pattern.args.regex) : null;
+
+    _setMinMaxInSwaggerSchema(schemaProperty, type, schema);
 
     schema.default = defaultValue ? JSON.stringify(defaultValue) : null;
 
     if (type === "object" && schemaProperty.$_terms.keys.length > 0) {
-        const requiredProperties = [];
-        const objectSchemaPropertyMap = {};
-
-        const objectSchemas = schemaProperty.$_terms.keys;
-        objectSchemas.forEach((objectSchema) => {
-            if (objectSchema.schema._flags.presence === "required") {
-                requiredProperties.push(objectSchema.key);
-            }
-            objectSchemaPropertyMap[objectSchema.key] = _getSchemaDefinitionForSwagger(
-                objectSchema.schema
-            );
-        });
-        schema.required = requiredProperties.length > 0 ? requiredProperties : null;
-        schema.properties = objectSchemaPropertyMap;
+        _setSwaggerPropsForObject(schemaProperty, schema);
     }
 
     if (type === "array") {
-        const itemSchema = schemaProperty.$_terms.items[0];
-        const hasItemSchema = schemaProperty.$_terms.items.length > 0;
-        schema.items = hasItemSchema ? _getSchemaDefinitionForSwagger(itemSchema) : {};
+        _setSwaggerPropsForArray(schemaProperty, schema);
     }
 
     Object.keys(schema).forEach((key) => schema[key] === null && delete schema[key]);
@@ -78,33 +96,36 @@ function _getSchemaDefinitionForSwagger(schemaProperty) {
     return schema;
 }
 
+function _getAllSwaggerParamsFromValidationSchema(schema, paramIn) {
+    return Object.keys(schema).map((key) => {
+        const schemaProperty = schema[key];
+        const schemaDefinition = _getSchemaDefinitionForSwagger(schemaProperty);
+        const isRequired = schemaProperty._flags.presence === "required";
+        return {
+            name: key,
+            in: paramIn,
+            required: isRequired,
+            schema: schemaDefinition
+        };
+    });
+}
+
 function schemaToSwaggerRequestParameters(validationSchema) {
     const parameters = [];
     if (!validationSchema) return parameters;
 
-    const { params, query, body, headers } = validationSchema;
-
-    const parameterKeyMap = {
-        path: params,
+    const {
         query,
         body,
-        headers
-    };
+        params: path,
+        headers: header
+    } = validationSchema;
+    const parameterKeyMap = { query, body, path, header };
 
-    Object.keys(parameterKeyMap).forEach((paramIn) => {
-        const schema = parameterKeyMap[paramIn];
+    Object.keys(parameterKeyMap).forEach((paramLocation) => {
+        const schema = parameterKeyMap[paramLocation];
         if (schema) {
-            const swaggerParams = Object.keys(schema).map((key) => {
-                const schemaProperty = schema[key];
-                const schemaDefinition = _getSchemaDefinitionForSwagger(schemaProperty);
-                const isRequired = schemaProperty._flags.presence === "required";
-                return {
-                    name: key,
-                    in: paramIn,
-                    required: isRequired,
-                    schema: schemaDefinition
-                };
-            });
+            const swaggerParams = _getAllSwaggerParamsFromValidationSchema(schema, paramLocation);
             parameters.push(...swaggerParams);
         }
     });
